e50cd2ecf01b1ed64d7ae2ed751859bace88999f
Brijesh Gupta <52522784+Brijesh739837@users.noreply.github.com>
2019-07-15 08:23:20 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,8 @@
+'''a=int(input("Enter first number="))
+b=int(input("Enter second number="))
+c=a+b
+print(c)'''
+a=5
+b=7
+c=a+b
+print(c)
---
+++ calculator.py
@@ -0,0 +1,9 @@
+a=int(input("Enter First number="))
+b=int(input("Enter second number="))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+h=a%b
+print("\t",c,d,e,f,g,h,end=".")
